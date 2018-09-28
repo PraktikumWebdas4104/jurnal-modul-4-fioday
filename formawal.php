bafdfa2522dc0e3ac7c4c420a7e5de35e6d26a63
Dayana Alfionisya <43520177+fioday@users.noreply.github.com>
2018-09-28 09:31:39 +0700

    Form Awal
    Merupakan tampilan awal berisi Log In dan Password

--- formawal.php
+++ formawal.php
@@ -1,5 +1,31 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>Login Disini</h2>
+ <form action="login.php" method="post">
+ 	username: <input type="text" name="username">
+
+ 	password: <input type="password" name="password">
+
+ 	<input type="submit" value="masuk">
+ </form>
+</body>
+</html>
+
 <?php
+session_start();
 
+$username=$_POST["username"];
+$password=$_POST["password"];
 
+if ($username == "dayana" and $password=="dayana") {
+
+	$_SESSION["username"]=$username;
+	header("location: proses.php");
+}else{
+	echo ("tidak login");
+}
 
 ?>
