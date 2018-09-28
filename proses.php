21019a7283dfe2660651b1163cd06a99cefb0a82
Dayana Alfionisya <43520177+fioday@users.noreply.github.com>
2018-09-28 10:08:14 +0700

    Proses
    Tampilan proses checkbox dan upload foto

--- proses.php
+++ proses.php
@@ -1,4 +1,23 @@
-<?php
+<h1> <center> HOBBY </h1> </center>
 
 
-?>
+<form>
+	<tr>
+	<td> Hobby: </td><br>
+
+	<td> <input type="checkbox" name="Hobby[]" value="Berenang"> Berenang </td><br>
+	<td> <input type="checkbox" name="Hobby[]" value="Main Basket"> Main Basket </td><br>
+	<td> <input type="checkbox" name="Hobby[]" value="Dance"> Dance </td><br>
+	<td> <input type="checkbox" name="Hobby[]" value="Jalan-Jalan"> Jalan-Jalan </td><br>
+	<td> <input type="checkbox" name="Hobby[]" value="Belanja"> Belanja </td><br><br>
+
+
+	<tr>
+	<td>Foto: </td><br>
+	<td><input type="file" name="dokumen" required></td>
+	<tr><td><input type="submit" name="upload"  value="upload"></td></tr><br><br>
+
+	<input type="submit" name="Send">
+	<input type="submit" name="submit" value="delete">
+</tr>
+</form>
